448037c0fa3ddbdc6ff07c5c314d0ade965a4d7c
Ben Yanke <ben@benyanke.com>
2018-12-11 22:02:27 -0600

    updating env vars

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 export AWS_KEY_ID='key1'
 export AWS_SECRET_KEY='key2'
 
-export URL_1='Url1Val'
-export URL_2='twoval'
-export URL_3='thisis3'
+export URL_1='https://www.irs.gov/pub/irs-soi/eo1.csv'
+export URL_2='https://www.irs.gov/pub/irs-soi/eo2.csv'
+export URL_3='https://www.irs.gov/pub/irs-soi/eo3.csv'
+export URL_4='https://www.irs.gov/pub/irs-soi/eo4.csv'
 export DATA_BUCKET='s3here'
 
 go fmt && go build && ./bmf-monitor
