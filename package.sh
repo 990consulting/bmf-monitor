b5f865407353669b8f8117e82a279f5555538283
Ben Yanke <ben@benyanke.com>
2018-12-11 21:53:35 -0600

    Adding error hiding to noncritical rm

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 
 # Run this to create a zip file which can be uploaded to s3
 
-rm lambda-package.zip
+rm lambda-package.zip 2> /dev/null
 GOOS=linux go build -o bmf-monitor main.go
 zip lambda-package.zip ./bmf-monitor
