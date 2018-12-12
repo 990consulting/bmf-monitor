fafb3ddacc471730e3e6b202690d53e6d45a46d5
Ben Yanke <ben@benyanke.com>
2018-12-11 22:26:16 -0600

    Adding alerting email, minor fixes (#2)
    * Adding error hiding to noncritical rm
    
    * updating env vars
    
    * Adding alert email
    
    * Added alert email
    
    * updating comment

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 export AWS_KEY_ID='key1'
 export AWS_SECRET_KEY='key2'
 
-export URL_1='Url1Val'
-export URL_2='twoval'
-export URL_3='thisis3'
+export URL_1='https://www.irs.gov/pub/irs-soi/eo1.csv'
+export URL_2='https://www.irs.gov/pub/irs-soi/eo2.csv'
+export URL_3='https://www.irs.gov/pub/irs-soi/eo3.csv'
+export URL_4='https://www.irs.gov/pub/irs-soi/eo4.csv'
 export DATA_BUCKET='s3here'
+export ALERT_EMAIL='example@example.com'
 
 go fmt && go build && ./bmf-monitor
 
