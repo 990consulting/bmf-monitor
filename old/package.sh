79452c183f65a7fcbf9a26e6ba201d18dc7516a9
Ben Yanke <ben@benyanke.com>
2018-12-13 22:56:09 -0600

    Removing old golang assets

--- old/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run this to create a zip file which can be uploaded to s3
-
-rm lambda-package.zip 2> /dev/null
-GOOS=linux go build -o bmf-monitor main.go
-zip lambda-package.zip ./bmf-monitor
